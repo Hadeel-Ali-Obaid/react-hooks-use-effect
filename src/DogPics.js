31adfa6f1ec13be2d27ef11bc70a3f364e5e573a
Hadeel Obaid <hadeelobaid71@gmail.com>
2024-03-02 21:04:12 +0300

    Practice

--- src/DogPics.js
+++ src/DogPics.js
@@ -5,15 +5,13 @@ import React, { useState, useEffect } from "react";
 function DogPics() {
   const [images, setImages] = useState([]);
 
-  useEffect(() => {
-    console.log("useEffect");
-    fetch("https://dog.ceo/api/breeds/image/random/3")
-      .then((r) => r.json())
-      .then((data) => {
-        console.log("setState");
-        setImages(data.message);
-      });
-  });
+useEffect(() => {
+  fetch("https://dog.ceo/api/breeds/image/random/3")
+    .then((r) => r.json())
+    .then((data) => {
+      setImages(data.message);
+    });
+}, []);
 
   console.log("render");
 
